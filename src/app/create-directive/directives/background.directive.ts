b57e80662333d7385a748bb22baf9ff0644dbb97
katerynabugaieva <kateryna.bugaieva@satellytes.com>
2019-10-02 17:52:54 +0200

    sendings the parameters to directive

--- src/app/create-directive/directives/background.directive.ts
+++ src/app/create-directive/directives/background.directive.ts
@@ -4,24 +4,30 @@ import {
   OnInit,
   Renderer2,
   HostListener,
-  HostBinding
+  HostBinding,
+  Input
 } from "@angular/core";
 
 @Directive({
   selector: "[create-directiveBackground]"
 })
 export class BackgroundDirective implements OnInit {
+  @Input() hoverColor: string = "green";
+  @Input() defaultColor: string = "transparent";
+
   @HostBinding("style.backgroundColor") background: string;
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.background = this.defaultColor;
+  }
 
   @HostListener("mouseenter") mouseEnter() {
     // this.element.nativeElement.style.backgroundColor = "red";
-    this.background = "red";
+    this.background = this.hoverColor;
   }
   @HostListener("mouseleave") mouseLeave() {
     //this.element.nativeElement.style.backgroundColor = "transparent";
-    this.background = "transparent";
+    this.background = this.defaultColor;
   }
 }
