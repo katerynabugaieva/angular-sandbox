13cf75aed2b40da305a9d38332294fc1a9ac0a45
katerynabugaieva <kateryna.bugaieva@satellytes.com>
2019-10-02 17:45:26 +0200

    update @HostBInding directives

--- src/app/create-directive/directives/background.directive.ts
+++ src/app/create-directive/directives/background.directive.ts
@@ -3,21 +3,25 @@ import {
   ElementRef,
   OnInit,
   Renderer2,
-  HostListener
+  HostListener,
+  HostBinding
 } from "@angular/core";
 
 @Directive({
   selector: "[create-directiveBackground]"
 })
 export class BackgroundDirective implements OnInit {
+  @HostBinding("style.backgroundColor") background: string;
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {}
 
   @HostListener("mouseenter") mouseEnter() {
-    this.element.nativeElement.style.backgroundColor = "red";
+    // this.element.nativeElement.style.backgroundColor = "red";
+    this.background = "red";
   }
   @HostListener("mouseleave") mouseLeave() {
-    this.element.nativeElement.style.backgroundColor = "transparent";
+    //this.element.nativeElement.style.backgroundColor = "transparent";
+    this.background = "transparent";
   }
 }
