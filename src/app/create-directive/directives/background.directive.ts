8a08c9b05774c74e4c22228f3eabef079ba68e25
katerynabugaieva <kateryna.bugaieva@satellytes.com>
2019-10-02 17:36:28 +0200

    renderer directives

--- src/app/create-directive/directives/background.directive.ts
+++ src/app/create-directive/directives/background.directive.ts
@@ -1,12 +1,19 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: "[create-directiveBackground]"
 })
 export class BackgroundDirective implements OnInit {
-  constructor(private element: ElementRef) {}
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = "red";
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      "background-color",
+      "green"
+    );
+
+    this.renderer.addClass(this.element.nativeElement, "white-text");
+    // this.element.nativeElement.style.backgroundColor = "red";
   }
 }
