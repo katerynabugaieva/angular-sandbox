2b707ee73da69b77360332b48150926951ad0229
katerynabugaieva <sonnensmile@gmail.com>
2019-10-07 12:15:45 +0200

    add pipes

--- src/app/car-filter.pipe.ts
+++ src/app/car-filter.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class CarFilterPipe implements PipeTransform {
   transform(carList, searchString: string, fieldName: string): any {
+    console.log("here");
     if (carList.length === 0 || searchString === "") return carList;
 
     return carList.filter(
