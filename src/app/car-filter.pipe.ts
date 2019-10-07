b52e175861d3c5c1caee45760e5d6af90cba6fd3
katerynabugaieva <sonnensmile@gmail.com>
2019-10-07 12:24:49 +0200

    customization

--- src/app/car-filter.pipe.ts
+++ src/app/car-filter.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: "carFilter"
+  name: "carFilter",
+  pure: false
 })
 export class CarFilterPipe implements PipeTransform {
   transform(carList, searchString: string, fieldName: string): any {
-    console.log("here");
     if (carList.length === 0 || searchString === "") return carList;
 
     return carList.filter(
