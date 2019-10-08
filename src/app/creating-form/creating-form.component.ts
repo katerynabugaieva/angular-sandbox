6f15864396f66cdde3c1814bbcf4697c44227058
katerynabugaieva <sonnensmile@gmail.com>
2019-10-08 09:34:08 +0200

    view child form

--- src/app/creating-form/creating-form.component.ts
+++ src/app/creating-form/creating-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -7,6 +7,7 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./creating-form.component.css"]
 })
 export class CreatingFormComponent {
+  @ViewChild("form", { static: false }) form: NgForm;
   answers = [
     {
       type: "yes",
@@ -17,7 +18,7 @@ export class CreatingFormComponent {
       text: "no"
     }
   ];
-  submitForm(form: NgForm) {
-    console.log(form);
+  submitForm() {
+    console.log(this.form);
   }
 }
