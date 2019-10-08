232058401fabcd1a6b37cca460cdbfe81f31caee
katerynabugaieva <sonnensmile@gmail.com>
2019-10-08 16:35:18 +0200

    adding headers

--- src/app/http-server/http-cars.service.ts
+++ src/app/http-server/http-cars.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers } from "@angular/http";
 
 @Injectable()
 export class HttpCarsService {
   constructor(private http: Http) {}
 
   getCars() {
+    const headers = new Headers({
+      "Content-Type": "application/json; charset=utf8"
+    });
     return this.http
-      .get("http://localhost:3000/cars")
+      .get("http://localhost:3000/cars", { headers })
       .map((response: Response) => response.json());
   }
 
   addCar(carName: string) {
+    const headers = new Headers({
+      "Content-Type": "application/json; charset=utf8"
+    });
     const data = {
       carName: carName,
       color: "blue"
     };
     return this.http
-      .post("http://localhost:3000/cars", data)
+      .post("http://localhost:3000/cars", data, { headers })
       .map((response: Response) => response.json());
   }
 
