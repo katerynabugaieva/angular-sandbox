51c161d755403c93b55581209ce90e8f26c3d1f0
katerynabugaieva <sonnensmile@gmail.com>
2019-10-09 10:40:03 +0200

    programm navigation

--- src/app/routing/home-page/home-page.component.ts
+++ src/app/routing/home-page/home-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.css"]
 })
 export class HomePageComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  openCarsPage() {
+    this.router.navigate(["cars"], { relativeTo: this.route });
   }
-
 }
