1fcb39a70a00d2d42bc8b64dd25353c04bf25689
katerynabugaieva <sonnensmile@gmail.com>
2019-10-10 11:05:02 +0200

    creating common module pr review

--- src/app/modules/modules-cars-page/modules-cars.module.ts
+++ src/app/modules/modules-cars-page/modules-cars.module.ts
@@ -4,15 +4,11 @@ import { ModulesCarsService } from "../modules-cars.service";
 import { CommonModule } from "@angular/common";
 import { ModulesCarPageComponent } from "./modules-car-page/modules-car-page.component";
 import { ModulesCarsRoutingModule } from "./modules-car-routing.module";
-import { ColorDirective } from "../shared/color.directives";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [
-    ModulesCarsPageComponent,
-    ModulesCarPageComponent,
-    ColorDirective
-  ],
-  imports: [CommonModule, ModulesCarsRoutingModule],
+  declarations: [ModulesCarsPageComponent, ModulesCarPageComponent],
+  imports: [CommonModule, ModulesCarsRoutingModule, SharedModule],
   providers: [ModulesCarsService]
 })
 export class ModulesCarsModule {}
